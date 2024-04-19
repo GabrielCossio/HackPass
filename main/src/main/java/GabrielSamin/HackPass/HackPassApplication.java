046de4bf2a1dae0d1377d8bb846b622dbe84ebda
samin2668 <44666180+samin2668@users.noreply.github.com>
2024-04-19 18:26:12 -0500

    Used SpringInit to create new springboot project

---
+++ main/src/main/java/GabrielSamin/HackPass/HackPassApplication.java
@@ -0,0 +1,13 @@
+package GabrielSamin.HackPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackPassApplication.class, args);
+	}
+
+}
---
+++ main/src/test/java/GabrielSamin/HackPass/HackPassApplicationTests.java
@@ -0,0 +1,13 @@
+package GabrielSamin.HackPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
