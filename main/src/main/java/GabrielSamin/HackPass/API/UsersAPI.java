efb7911625f2fee53dc23a047e19ef7d00341946
samin2668 <44666180+samin2668@users.noreply.github.com>
2024-04-30 17:01:15 -0500

    -Interfaces for service and api layer created -class files implement these interfaces -file names changes to reflect these changes

--- main/src/main/java/GabrielSamin/HackPass/API/UsersAPI.java
+++ main/src/main/java/GabrielSamin/HackPass/API/UsersAPI.java
@@ -2,67 +2,17 @@ package GabrielSamin.HackPass.API;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import GabrielSamin.HackPass.DTO.UsersDTO;
-import GabrielSamin.HackPass.Service.UserService;
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-
-@RestController
-@RequestMapping("/")
-public class UsersAPI {
-    @Autowired
-    UserService userService;
-
-    @GetMapping("users")
-    public ResponseEntity<List<UsersDTO>> getAllUsers() {
-        List<UsersDTO> l = userService.getAllUsers();
-        return new ResponseEntity<List<UsersDTO>>(l, HttpStatus.OK);
-    }
-    
-    @GetMapping("user/{id}")
-    public ResponseEntity<UsersDTO> getUserById(@PathVariable String id) {
-        UsersDTO dto = userService.findById(id);
-        return new ResponseEntity<UsersDTO>(dto, HttpStatus.OK);
-    }
-
-    @PostMapping("user")
-    @CrossOrigin(origins = "http://localhost:5173")
-    public ResponseEntity<UsersDTO> addUser(@RequestBody @Valid UsersDTO entity) {
-        entity = userService.addUser(entity);
-        return new ResponseEntity<UsersDTO>(entity, HttpStatus.CREATED);
-    }
-
-    @PutMapping("user/{id}")
-    public ResponseEntity<UsersDTO> updateCheckinStatus(@PathVariable String id) {
-        UsersDTO updatedUsersDTO = userService.updateCheckinStatus(id);
-
-        if (updatedUsersDTO != null) { 
-            return new ResponseEntity<UsersDTO>(updatedUsersDTO, HttpStatus.OK); //return 200 OK w/ updated DTO
-        } else{
-            return new ResponseEntity<UsersDTO>(updatedUsersDTO, HttpStatus.NOT_FOUND); //404 user not found
-        }
-    }
-
-    @DeleteMapping
-    public ResponseEntity<String> deleteUser(@PathVariable String Id){
-        if(userService.deleteUser(Id)){
-            return new ResponseEntity<String>("User " + Id + " Has Been Deleted", HttpStatus.OK);
-        } else{
-            return new ResponseEntity<String>("User " + Id + " Does Not Exist", HttpStatus.NOT_FOUND);
-        }
-
-    }
+public interface UsersAPI {
+    public ResponseEntity<List<UsersDTO>> getAllUsers();
+    public ResponseEntity<UsersDTO> getUserById(@PathVariable String id);
+    public ResponseEntity<UsersDTO> addUser(@RequestBody @Valid UsersDTO entity);
+    public ResponseEntity<UsersDTO> updateCheckinStatus(@PathVariable String id);
+    public ResponseEntity<String> deleteUser(@PathVariable String Id);
 }
---
+++ main/src/main/java/GabrielSamin/HackPass/Service/UserServiceImp.java
@@ -0,0 +1,68 @@
+package GabrielSamin.HackPass.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import GabrielSamin.HackPass.DTO.UsersDTO;
+import GabrielSamin.HackPass.Document.Users;
+import GabrielSamin.HackPass.Repository.UserRepo;
+
+@Service
+public class UserServiceImp implements UserService{
+    @Autowired
+    UserRepo userRepo;
+
+    public List<UsersDTO> getAllUsers(){
+        List<Users> usersList = userRepo.findAll();
+        return usersList.stream().map(u->u.DocToDTO()).collect(Collectors.toList());
+    }
+
+    public UsersDTO findById(String id){
+        ObjectId ID = new ObjectId(id);
+        Users u = userRepo.findBy_id(ID);
+        
+        return u.DocToDTO();
+    }
+
+    public UsersDTO addUser(UsersDTO u){
+        
+        Users newUser = u.DTOToDoc();
+        if(userRepo.count()>=500){
+            newUser.setWaitlistStatus(true);
+        }
+        u = userRepo.save(newUser).DocToDTO();
+
+        return u;
+    }
+
+    public UsersDTO updateCheckinStatus (String id) {
+        Optional<UsersDTO> optionalUser = Optional.of(this.findById(id));
+
+        if(optionalUser.isPresent()){
+            UsersDTO userDTO = optionalUser.get();
+            Users user = userDTO.DTOToDoc();
+            user.setCheckinStatus(true);
+            Users updatedUser = userRepo.save(user);
+            return updatedUser.DocToDTO();
+        } else {
+            return null;
+        }
+
+    }
+
+    public Boolean deleteUser(String id){
+        Optional<UsersDTO> optionalUser = Optional.of(this.findById(id));
+        if(optionalUser.isPresent()){
+            Users userToDelete = optionalUser.get().DTOToDoc();
+            userRepo.delete(userToDelete);
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
